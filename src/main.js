5208dfdbfc134a92c358946ea9ced6dd2038b197
Fiddry <wenyan@2019.cqut.edu.cn>
2021-06-12 14:39:52 +0800

    1.3.4

--- src/main.js
+++ src/main.js
@@ -4,13 +4,12 @@ import router from "./router";
 import store from "./store";
 import installElementPlus from "./plugins/element";
 import "./assets/css/icon.css";
-import { VueEcharts } from "vue3-echarts";
+
 const app = createApp(App);
-// import "echarts-gl";
+
 installElementPlus(app);
-import "echarts-gl";
+
 app
-  .use(VueEcharts)
   .use(store)
   .use(router)
   .mount("#app");
--- src/views/default/index.vue
+++ src/views/default/index.vue
@@ -1,14 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <v-chart></v-chart>
+  </div>
 </template>
 
 <script>
-export default {};
+// eslint-disable-next-line no-unused-vars
+import vChart from "./components/echartdemo.vue";
+export default {
+  components: {
+    vChart,
+  },
+};
 </script>
 
-<style>
-.schart {
-  width: 100%;
-  height: 300px;
-}
-</style>
+<style></style>
