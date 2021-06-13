d7c31e2a36cbbe0f0151b1dc2e1547278b806285
Fiddry <wenyan@2019.cqut.edu.cn>
2021-06-13 15:21:18 +0800

    1.6

--- src/views/costom/components/buygoods.vue
+++ src/views/costom/components/buygoods.vue
@@ -28,7 +28,6 @@ export default {
   data() {
     return {
       goodsItems: this.$store.state.goodsItems,
-      boughtGoods: this.$store.state.boughtGoods,
       multipleSelection: [],
     };
   },
